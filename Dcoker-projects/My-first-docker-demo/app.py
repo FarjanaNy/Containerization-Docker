93f4ed2588b279a9e5061e6ffdf83578deb82261
FarjanaNy <farjanajamal205@gmail.com>
2025-11-25 13:22:52 +0530

    Update app.py

--- Dcoker-projects/My-first-docker-demo/app.py
+++ Dcoker-projects/My-first-docker-demo/app.py
@@ -1,1 +1,13 @@
+# app.py
+import os
 
+name = os.getenv("NAME", "Friend")
+
+print(f"Hello {name} 👋")
+print("I am running inside a Docker container.")
+
+with open("message.txt", "w") as f:
+    f.write(f"This file was created inside a container. Hello {name}!")
+
+print("File created: message.txt")
+print("Container will stop now. Bye!")
